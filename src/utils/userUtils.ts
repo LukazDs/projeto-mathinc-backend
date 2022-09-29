4b15a858d8f96260a6a94ff3326490f7e48ec300
LukazDs <lukazds2016@gmail.com>
2022-09-28 22:19:55 -0300

    feat: create user utils

---
+++ src/utils/userUtils.ts
@@ -0,0 +1,12 @@
+import { Users } from "@prisma/client";
+
+export type IUser = Omit<Users, "id">;
+
+export interface IUserCreationReg extends IUser {
+  confirmedPassword: string;
+}
+
+export type IUserCreationLogin = Omit<
+  IUser,
+  keyof { name: string; imageUrl: string }
+>;
